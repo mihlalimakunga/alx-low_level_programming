f5fb6f9d799c186cfae09f9757a049b2f8b1171e
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 07:43:40 -0800

    Program that prints the size of various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always zero
+ */
+int main()
+{
+	char charType;
+	int integerType;
+	long longType;
+	short shortType;
+
+	print("Size of int: %ld bytes\n",sizeof(intType));
+	print("Size of char: %ld bytes\n",sizeof(charType));
+	print("Size of long: %ld bytes\n",sizeof(longType));
+	print("Size of short: %ld bytes\n",sizeof(shortType));
+
+	return (0)
+}
