a7c40f175c274568922f17092cfc7e5cad7082d9
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 11:55:25 -0800

    Program to find size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,14 +4,18 @@
  *
  * Return: always zero
  */
-int main(void)
+int main()
 {
-	int i;
-	double d;
-	char c;
-	
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+	char charType;
+	int intType;
+	long int long_intType;
+	long long int long_long_ingType;
+	float floatType;
+
+	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of long int: %zu bytes\n", sizeof(long_intType));
+	printf("Size of long long int: %zu bytes\n", sizeof(long_long_intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
 	return (0)
 }
