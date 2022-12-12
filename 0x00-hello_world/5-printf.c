076879a5f3f62229a7b8314a4a97421006a8dce3
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 06:31:08 -0800

    incorporated the Betty documentation style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(void)
-	/* Program that will print a text using printf function */
+	/**
+	 * Program that will print a text using printf function
+	 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
