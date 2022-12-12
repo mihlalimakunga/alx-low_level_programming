0362c47d582c84d201775020a332fc0b9709519b
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 06:44:12 -0800

    documentation using betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ */
 int main(void)
-	/**
-	 * main: entry point
-	 * Program that will print a text using printf function
-	 * return: zero
-	 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
