90ca8f6881d55be09a9464dc075ed36724f115ec
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 08:10:11 -0800

    Deletion of words in the use of sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int main()
 	long longType;
 	short shortType;
 
-	printf("Size of int: %ld bytes\n",sizeof(intType));
-	printf("Size of char: %ld bytes\n",sizeof(charType));
-	printf("Size of long: %ld bytes\n",sizeof(longType));
-	printf("Size of short: %ld bytes\n",sizeof(shortType));
+	printf("%ld\n",sizeof(intType));
+	printf("%l\n",sizeof(charType));
+	printf("%ld\n",sizeof(longType));
+	printf("%ld\n",sizeof(shortType));
 	return (0)
 }
