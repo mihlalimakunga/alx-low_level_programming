55c98e850302e3c7b5ac0efdbef79ddd4f72df95
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 13:45:13 -0800

    Program that to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  *
- * Return: always one (error)
+ * Return: always 1
  */
 int main(void)
 {
-	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(2, err, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
