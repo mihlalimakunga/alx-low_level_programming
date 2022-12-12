92452fcc7a31d0f7c55c9cd9db1aa778bc2bf35c
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 06:56:05 -0800

    edited the 4-puts.c file to comply with the betty documentation and coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"); 
-	return 0; 
+	return (0); 
 }  
