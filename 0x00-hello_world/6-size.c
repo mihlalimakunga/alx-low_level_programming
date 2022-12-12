6455e4c618b92ae28dee191a26619472d4745e1b
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 11:27:02 -0800

    removed space between last printf line and return line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,5 @@ int main(void)
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
 	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
-
 	return (0)
 }
