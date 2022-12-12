68d232d6c73fa630254ccaed60c6ff9471eb2e7c
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 13:22:37 -0800

    Program that prints to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always one (error)
+ */
+int main(void)
+{
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, err, 59);
+	return (1);
+}
