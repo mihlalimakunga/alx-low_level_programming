c5ea0d4142b95ffd0c5b235cb0764eb59b98958f
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-12 06:37:02 -0800

    Description of main function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(void)
 	/**
+	 * main: entry point
 	 * Program that will print a text using printf function
+	 * return: zero
 	 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
