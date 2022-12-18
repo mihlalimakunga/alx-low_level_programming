d856552be32da2c033addaf569fbc063c3c4984e
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-18 11:37:09 -0800

    Added spaces between relational operators

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	char x;
 
-	for (x='a'; x<='z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
 	
-	for (x='A'; x<='Z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(x);
 	}
-
+	
 	putchar('\n');
 
 	return (0);
