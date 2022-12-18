5b848db5d1aa0bcb7dd35457668cee8abe3a7269
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-18 08:15:11 -0800

    Added (n % 10) on 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,17 +24,17 @@ int main(void)
 
 					if (n > 5) 
 					{
-						printf("Last digit of n % 10 is %d and is greater than 5\n", n);
+						printf("Last digit of (n % 10) is %d and is greater than 5\n", n, (n % 10));
 					}
 
 					if (n == 0)
 					{
-						printf("Last digit of n % 10 is %d and is 0\n", n);
+						printf("Last digit of (n % 10) is %d and is 0\n", n, (n % 10));
 					}
 
 					if (n < !6)
 					{
-						printf("Last digit of n % 10 is %d and is less than 6 and not 0\n", n);
+						printf("Last digit of (n % 10) is %d and is less than 6 and not 0\n", n, (n % 10));
 					}
 
 					return (0);
