50d33d3acb5c122eb2290653843a99bbd895d066
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-18 02:58:23 -0800

    Incorporated "n" instead of previous "int"

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,17 +24,17 @@ int main(void)
 
 					if (n > 5) 
 					{
-						printf("Last digit of int is %d and is greater than 5\n");
+						printf("Last digit of n is %d and is greater than 5\n", n);
 					}
 
 					if (n == 0)
 					{
-						printf("Last digit of int is %d and is 0\n");
+						printf("Last digit of n is %d and is 0\n", n);
 					}
 
 					if (n <! 6)
 					{
-						printf("Last digit of int is %d and is less than 6 and not 0\n");
+						printf("Last digit of n is %d and is less than 6 and not 0\n", n);
 					}
 
 					return (0);
