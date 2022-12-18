25f4bf1f6502a1f7cee6f764f1d77fe903e9f770
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-18 11:58:53 -0800

    Removed trailing whitespace

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,17 +9,17 @@
 int main(void)
 {
 	char x;
+	
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
 
-	for(x = 'a'; x <= 'z'; x++)
-	{
-		putchar(x);
-	}
-	
-	for(x = 'A'; x <= 'Z'; x++)
-	{
-		putchar(x);
-	}
-	
 	putchar('\n');
 
 	return (0);
