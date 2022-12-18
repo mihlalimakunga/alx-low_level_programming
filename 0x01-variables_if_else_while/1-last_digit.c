78a9c816dc589aba819a9acc1fdf6dc0f3000a8b
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-18 03:13:57 -0800

    Removed trailing whitespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,19 +24,18 @@ int main(void)
 
 					if (n > 5) 
 					{
-						printf("Last digit of n is %d and is greater than 5\n", n);
+						printf("Last digit of %d is %d and is greater than 5\n", n);
 					}
 
 					if (n == 0)
 					{
-						printf("Last digit of n is %d and is 0\n", n);
+						printf("Last digit of %d is %d and is 0\n", n);
 					}
 
-					if (n <! 6)
+					if (n < !6)
 					{
-						printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+						printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
 					}
 
 					return (0);
-
 }
