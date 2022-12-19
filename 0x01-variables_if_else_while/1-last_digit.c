060eadc27fb3d769896053afc777c48c4ec578d0
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-19 00:20:54 -0800

    Used if else statement as one block

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -37,11 +37,9 @@ int main(void)
 					if ((n < 6) && !0)
 					{
 						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
-					}
 
-					else
-					{
-						printf("Last digit of %d id %d and is 0\n", n, x);
+						else
+							printf("Last digit of %d is %d and is 0\n", n, x);
 					}
 
 					return (0);
