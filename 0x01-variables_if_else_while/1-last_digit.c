46dbed0160731b676b6d87cbd9cab60b91013cdd
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-19 02:12:01 -0800

    Used if statements to print out the last digit of a string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,19 +24,19 @@ int main(void)
 
 				x = n % 10;
 
+				if (n == 0)
+				{
+					printf("Last digit of %d is %d and is 0\n", n, x);
+				}
+
 				if (n > 5)
 				{
 					printf("Last digit of %d is %d and is greater than 5\n", n, x);
 				}
-				
-				else if ((n < 6) && !0)
+
+				if ((n < 6) && !6)
 				{
 					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
-				}
-
-				else if (n == 0)
-				{
-					printf("Last digit of %d is %d and is 0\n", n, x);
 				}
 
 				return (0);
