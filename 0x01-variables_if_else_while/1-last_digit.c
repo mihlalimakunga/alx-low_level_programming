17d78bdda152055e2d0b037a949621ada47842e9
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-19 00:12:54 -0800

    Added and else statement on the last if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -39,5 +39,10 @@ int main(void)
 						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 					}
 
+					else
+					{
+						printf("Last digit of %d id %d and is 0\n", n, x);
+					}
+
 					return (0);
 }
