46884e21a56eb380d5c0e55ea09b270feeb18ed0
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-18 23:43:47 -0800

    Inserted the modulo operator in my program to get the remainder as the last digit when dividing by 10

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 
 {
 
-		int n;
+		int n, x;
 
 
 
@@ -22,20 +22,22 @@ int main(void)
 
 				n = rand() - RAND_MAX / 2;
 
-					if (n > 5) 
+				x = n % 10;
+				
+				if (n > 5 
 					{
-						printf("Last digit of (n % 10) is %d and is greater than 5\n", n, (n % 10));
+						printf("Last digit of %d is %d and is greater than 5\n", n, x);
 					}
 
 					if (n == 0)
 					{
-						printf("Last digit of (n % 10) is %d and is 0\n", n, (n % 10));
+						printf("Last digit of %d is %d and is 0\n", n, x);
 					}
 
 					if (n < !6)
 					{
-						printf("Last digit of (n % 10) is %d and is less than 6 and not 0\n", n, (n % 10));
+						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 					}
 
 					return (0);
-}
+
