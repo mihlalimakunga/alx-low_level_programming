f219e5e17976fb32229d299e45744f4b025eef1f
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-19 00:55:11 -0800

    Removed the "x" variable in n=0 statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,16 +23,21 @@ int main(void)
 				n = rand() - RAND_MAX / 2;
 
 				x = n % 10;
-				
-				if (n > 5) 
-					{
-						printf("Last digit of %d is %d and is greater than 5\n", n, x);
-					}
 
-					if (n == 0)
-					{
-						printf("Last digit of %d is %d and is 0\n", n, x);
-					}
+				if (n > 5)
+				{
+					printf("Last digit of %d is %d and is greater than 5\n", n, x);
+				}
 
-					return (0);
+				if (n == 0)
+				{
+					printf("Last digit of %d is %d and is 0\n", n);
+				}
+
+				if ((n < 6) && !0)
+				{
+					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+				}
+
+				return (0);
 }
