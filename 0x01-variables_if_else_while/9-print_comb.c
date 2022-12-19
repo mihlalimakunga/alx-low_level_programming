571a308ccaf6e5be0c34be6631286c052a601239
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-19 08:38:20 -0800

    Program that  prints all possible combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always zero for success
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
