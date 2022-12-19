b131b24781e28bb3e472f2d7ec8a9f9e40a717d7
Mihlali Makunga <mmakunga2012@mysbf.co.za>
2022-12-19 03:51:07 -0800

    A program that prints all alphabets in small letters except for e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always zero (success)
+ */
+
+int main(void)
+
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; a++)
+	{
+		if (x != 'e' && x != 'q')
+
+			putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
